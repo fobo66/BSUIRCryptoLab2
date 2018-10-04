0e611e5e90da6294a87b3c6b40d54c9dae1d532c
Andrey Mukamolov <fobo66@protonmail.com>
2018-10-04 22:58:38 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    application
+}
+
+application {
+    mainClassName = "io.fobo66.crypto.Lab2"
+}
+
+allprojects{
+    repositories {
+        mavenCentral()
+    }
+}
+
+dependencies {
+    implementation("commons-cli:commons-cli:1.4")
+}
---
+++ src/main/java/io/fobo66/crypto/Lab2.java
@@ -0,0 +1,7 @@
+package io.fobo66.crypto;
+
+public class Lab2 {
+    public static void main(String[] args) {
+        System.out.println("RSA");
+    }
+}
