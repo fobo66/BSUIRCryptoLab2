fda25a09cffadc6e6c2aa655c79793806d7954d5
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-03 23:04:20 +0300

    Converted algorithm to Kotlin

--- src/main/java/dev/fobo66/crypto/RSA.kt
+++ src/main/java/dev/fobo66/crypto/RSA.kt
@@ -1,45 +1,37 @@
-package dev.fobo66.crypto;
+package dev.fobo66.crypto
 
-import java.math.BigInteger;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import java.math.BigInteger
+import java.util.*
+import java.util.concurrent.ThreadLocalRandom
 
-public class RSA {
-    private BigInteger p;
-    private BigInteger q;
-    private BigInteger N;
-    private BigInteger phi;
-    private BigInteger e;
-    private BigInteger d;
-    private int bitlength = 1024;
-    private Random r;
+class RSA {
+    private var p: BigInteger
+    private var q: BigInteger
+    private var N: BigInteger
+    private var phi: BigInteger
+    private var e: BigInteger
+    private var d: BigInteger
+    private val bitlength = 1024
+    private val r: Random
 
-    public RSA() {
-        r = ThreadLocalRandom.current();
-        p = BigInteger.probablePrime(bitlength, r);
-        q = BigInteger.probablePrime(bitlength, r);
-        N = p.multiply(q);
-        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-        e = BigInteger.probablePrime(bitlength / 2, r);
-        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0) {
-            e.add(BigInteger.ONE);
+    init {
+        r = ThreadLocalRandom.current()
+        p = BigInteger.probablePrime(bitlength, r)
+        q = BigInteger.probablePrime(bitlength, r)
+        N = p.multiply(q)
+        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE))
+        e = BigInteger.probablePrime(bitlength / 2, r)
+        while (phi.gcd(e) > BigInteger.ONE && e < phi) {
+            e.add(BigInteger.ONE)
         }
-        d = e.modInverse(phi);
+        d = e.modInverse(phi)
     }
 
-    public RSA(BigInteger e, BigInteger d, BigInteger N) {
-        this.e = e;
-        this.d = d;
-        this.N = N;
+    fun encrypt(message: ByteArray?): ByteArray {
+        return BigInteger(message).modPow(e, N).toByteArray()
     }
 
-    // Encrypt message
-    public byte[] encrypt(byte[] message) {
-        return (new BigInteger(message)).modPow(e, N).toByteArray();
-    }
-
-    // Decrypt message
-    public byte[] decrypt(byte[] message) {
-        return (new BigInteger(message)).modPow(d, N).toByteArray();
+    fun decrypt(message: ByteArray?): ByteArray {
+        return BigInteger(message).modPow(d, N).toByteArray()
     }
 }
