e66e3ce73377098e4fa356e795e0cbe67ace9638
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-02-12 12:08:09 +0200

    Updated Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("kotlin", "1.8.0")
+            version("kotlin", "1.8.10")
             library("cli", "org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
         }
     }
